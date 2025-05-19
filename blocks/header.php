8d24d5c9ab1fdf8151b25e0774fed6f2e025bf6c
ubska <alessandraubska@gmail.com>
2025-05-19 17:42:06 +0200

    change portfolio in registration

--- blocks/header.php
+++ blocks/header.php
@@ -4,7 +4,7 @@
         <ul>
             <li class="active"><a href="index.php">Home</a></li>
             <li><a href="about.php">About us</a></li>
-            <li><a href="#">Portfolio</a></li>
+            <li><a href="reg.php">Registration</a></li>
             <li><a href="#">News</a></li>
             <li class="btn"><a href="contacts.php">Contacts</a></li>
         </ul>
