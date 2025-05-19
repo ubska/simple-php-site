eafc88b465d6e5364f7f2063b63343ddbd41c94f
ubska <alessandraubska@gmail.com>
2025-05-19 18:20:11 +0200

    filtred input from form

--- lib/reg.php
+++ lib/reg.php
@@ -1,4 +1,6 @@
 <?php
-
-$login = $_POST['login'];
-echo $login;
+// La funzione filter_var() viene utilizzata per filtrare e sanitizzare i dati.
+$login = trim(filter_var($_POST['login'], FILTER_SANITIZE_SPECIAL_CHARS));
+$username = trim(filter_var($_POST['username'], FILTER_SANITIZE_SPECIAL_CHARS));
+$email = trim(filter_var($_POST['email'], FILTER_SANITIZE_SPECIAL_CHARS));
+$password = trim(filter_var($_POST['password'], FILTER_SANITIZE_SPECIAL_CHARS));
