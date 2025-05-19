df9763636493c5b727b15c2f516d8fd58943cd88
ubska <alessandraubska@gmail.com>
2025-05-19 16:51:20 +0200

    Initial commit: added static HTML/CSS mockup for future PHP integration

---
+++ contacts.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Website</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body>
+    <header class="container">
+        <span class="logo">logo</span>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about.html">About us</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">News</a></li>
+                <li class="btn"><a href="/contacts.html">Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container hero-contacts">
+        <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <img src="/img/Map.png" alt="">
+    </div>
+
+    <div class="feedback">
+        <div class="container">
+            <h2>Say hello</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing .</p>
+
+            <form>
+                <div class="inline">
+                    <div>
+                        <label>First Name</label>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <input type="text">
+                    </div>
+                </div>
+                <label>Email Address</label>
+                <input type="email" class="one-line">
+
+                <label>Message</label>
+                <textarea class="one-line"></textarea>
+
+                <button type="button">Get in touch</button>
+            </form>
+        </div>
+    </div>
+
+    <footer>
+        <div class="blocks container">
+            <div>
+                <span class="logo">logo</span>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            </div>
+            <div>
+                <h4>About us</h4>
+                <ul>
+                    <li>Zeux</li>
+                    <li>Portfolio</li>
+                    <li>Careers</li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Contact us</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                <p>+908 89097 890</p>
+            </div>
+        </div>
+        <hr>
+        <p>Copyright ® 2021 Lorem All rights Rcerved</p>
+    </footer>
+</body>
+
+</html>
