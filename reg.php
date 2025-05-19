95d4ffcb7bb8c26e71e584fcd84b7df9d45bedf8
ubska <alessandraubska@gmail.com>
2025-05-19 17:49:12 +0200

    refactored form inputs

---
+++ reg.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <?php require_once "blocks/header.php"; ?>
+
+    <div class="feedback">
+        <div class="container">
+            <h2>Registration</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing .</p>
+
+            <form>
+                <div class="inline">
+                    <div>
+                        <label>Login</label>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <label>Name</label>
+                        <input type="text">
+                    </div>
+                </div>
+                <label>Email Address</label>
+                <input type="email" class="one-line">
+
+                <label>Password</label>
+                <input type="email" class="one-line">
+
+                <button type="button">Sign Up</button>
+            </form>
+        </div>
+    </div>
+
+    <?php require_once "blocks/footer.php"; ?>
+</body>
+
+</html>
