2f8a4bf90096526dd245ce41ddea739c1ff408c6
ubska <alessandraubska@gmail.com>
2025-05-20 19:04:55 +0200

    feat: create trending.php page

---
+++ trending.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Website</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php require_once "blocks/header.php"; ?>
+
+        <div class="container trending">
+            <a href="#" class="see-all">SEE ALL</a>
+            <h3>Currently Trending Games</h3>
+
+            <div class="games">
+                <?php
+                // DB
+                require_once "lib/db.php";
+                // SQL
+                $sql = 'SELECT * FROM trending ORDER BY id DESC LIMIT 4';
+                $query = $pdo->prepare($sql);
+                $query->execute();
+                $games = $query->fetchAll(PDO::FETCH_OBJ);
+                foreach ($games as $el)
+                    echo ' <div class="block">
+                    <img src="img/' . $el->image . '" alt="">
+                    <span><img src="img/fire.svg" alt=""> ' . $el->followers . ' Followers</span>
+                </div>';
+                ?>
+
+
+            </div>
+        </div>
+
+
+
+        <?php require_once "blocks/footer.php"; ?>
+
+
+</body>
+
+</html>
