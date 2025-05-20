2270f24ba30c4f304c51ec31776fde494691ce52
ubska <alessandraubska@gmail.com>
2025-05-20 17:10:32 +0200

    create dashboard

---
+++ user.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <?php require_once "blocks/header.php"; ?>
+
+    <div class="feedback">
+        <div class="container">
+            <h2>Dashboard</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing .</p>
+
+
+        </div>
+    </div>
+
+    <?php require_once "blocks/footer.php"; ?>
+</body>
+
+</html>
