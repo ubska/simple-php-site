2e9517371a9003469650b703d49de618b0318690
ubska <alessandraubska@gmail.com>
2025-05-20 17:43:16 +0200

    show dashboard link for loggedIN users and login and registration links for guests

--- blocks/header.php
+++ blocks/header.php
@@ -4,8 +4,19 @@
         <ul>
             <li class="active"><a href="index.php">Home</a></li>
             <li><a href="about.php">About us</a></li>
-            <li><a href="reg.php">Registration</a></li>
-            <li><a href="auth.php">Login</a></li>
+
+            <?php
+            if (isset($_COOKIE['login'])) {
+                echo '<li><a href="user.php">Dashboard</a></li>';
+            } else {
+                echo '<li><a href="reg.php">Registration</a></li>
+            <li><a href="auth.php">Login</a></li>';
+            };
+
+            ?>
+
+
+
             <li class="btn"><a href="contacts.php">Contacts</a></li>
         </ul>
     </nav>
